e92450e9578d35fa8f71b24788297ad4bf7f14d5
System Administrator <root@Duncan.local>
2019-12-10 17:44:09 +0800

    kafka first commit

---
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -0,0 +1,24 @@
+package com.example.kafka;
+
+import com.example.kafka.producer.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+    @Autowired
+    private Sender sender;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        sender.send("Spring Kafka Producer and Consumer Example");
+    }
+
+}
